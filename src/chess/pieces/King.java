cdfe88994d8a50e7a985e8bd9ab2b371c1b7506b
tzuchaedahy <viniciuslovestwice@gmail.com>
2024-06-05 18:21:16 -0300

    feat: create place piece method and some chess pieces

---
+++ src/chess/pieces/King.java
@@ -0,0 +1,16 @@
+package src.chess.pieces;
+
+import src.board.Board;
+import src.chess.ChessPiece;
+import src.chess.Color;
+
+public class King extends ChessPiece {
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
